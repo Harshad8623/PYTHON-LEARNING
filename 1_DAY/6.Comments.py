d1b1735239253d9ae4bda58c07c52ec8e3bb1ce4
harshad8623 <dhuppeh@gmail.com>
2025-12-14 08:30:31 +0530

    Auto-update: 14-12-2025  8:30:31.43

---
+++ 1_DAY/6.Comments.py
@@ -0,0 +1,25 @@
+# Single line Comments
+
+
+
+# Python has no official multi-line comment syntax
+'''
+This is a multi-line comment
+which spans multiple lines.
+It is often used to provide detailed explanations
+or documentation for code sections.
+'''
+
+"""Another way to write multi-line comments
+is by using triple double quotes.
+This is also useful for documentation.
+"""
+
+"Hello Harshad"  # This is an inline comment
+
+print("Hello, World!")  # This prints a greeting message
+# This program calculates
+# the area of a circle
+# using the formula πr²
+
+
--- 2_DAY/practice3.py
+++ 2_DAY/practice3.py
@@ -1,4 +1,5 @@
 # Write a python program to grnerate a random number between 1000 and 9999
+
 import random
 random_number = random.randint(1000, 9999)
 print("Random number between 1000 and 9999 is:", random_number)
